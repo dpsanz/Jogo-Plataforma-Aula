3e9dd67030de01b88bb259a23aa7235aec6c8019
Daniel Sanchez <hd2242kin20@gmail.com>
2024-09-02 10:07:29 -0300

    aula2/09

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float horizontal;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+
+        this.rb.velocity = new Vector2(horizontal * 8f, rb.velocity.y);
+        Flip();
+        
+        /*
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Apertou Espaço");
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicou");
+        }
+        */
+
+        
+
+        
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    } 
+ }
